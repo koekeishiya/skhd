e85ac51691e5e9887221b4c6be4a00bbe16c4137
koekeishiya <aasvi93@hotmail.com>
2017-08-07 20:40:53 +0200

    v0.0.2

--- src/skhd.c
+++ src/skhd.c
@@ -28,7 +28,7 @@ extern bool CGSIsSecureEventInputSet();
 
 internal unsigned major_version = 0;
 internal unsigned minor_version = 0;
-internal unsigned patch_version = 1;
+internal unsigned patch_version = 2;
 internal char *config_file;
 struct hotkey *hotkeys;
 
